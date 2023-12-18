cecda2363e1e9a703dad73f558805867fe512e3f
popstarmav <cj.johnson2017@gmail.com>
2023-12-17 22:47:23 -0600

    sort alg

---
+++ print_array.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - Prints an array of integers.
+ * @array: The array to be printed.
+ * @size: Number of elements in @array.
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i = 0;
+
+    if (array == NULL)
+        return;
+
+    while (i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
