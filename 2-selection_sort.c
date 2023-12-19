e2ebbbcda28fa271c1c82b0174b3aa0a6296adc7
popstarmav <cj.johnson2017@gmail.com>
2023-12-19 11:17:00 -0600

    update sort algo

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,28 +8,30 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
+    if (array == NULL || size < 2)
+        return;
 
-	for (size_t i = 0; i < size - 1; ++i)
-	{
-		size_t min_index = i;
+    size_t i, j, min_index;
+    
+    for (i = 0; i < size - 1; ++i)
+    {
+        min_index = i;
 
-		for (size_t j = i + 1; j < size; ++j)
-		{
-			if (array[j] < array[min_index])
-				min_index = j;
-		}
+        for (j = i + 1; j < size; ++j)
+        {
+            if (array[j] < array[min_index])
+                min_index = j;
+        }
 
-		if (min_index != i)
-		{
-			/* Swap elements */
-			int tmp = array[i];
-			array[i] = array[min_index];
-			array[min_index] = tmp;
+        if (min_index != i)
+        {
+            /* Swap elements */
+            int tmp = array[i];
+            array[i] = array[min_index];
+            array[min_index] = tmp;
 
-			/* Print the array after each swap */
-			print_array(array, size);
-		}
-	}
+            /* Print the array after each swap */
+            print_array(array, size);
+        }
+    }
 }
